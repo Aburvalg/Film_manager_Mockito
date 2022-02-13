2e237ba8c631a376b73f7cffdd6c58bf70255a9d
Andrey <3451z@mail.ru>
2022-02-13 10:53:43 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -0,0 +1,77 @@
+package ru.netology.manager;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.netology.domain.Film;
+import ru.netology.repository.FilmRepository;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class FilmManager {
+    private Film[] films = new Film[0];
+    //private FilmRepository repository;
+    private int limitForShow = 10;
+
+//    public FilmManager(FilmRepository repository) {
+//        this.repository = repository;
+//    }
+
+    public void save(Film film) {
+        int length = films.length + 1;
+        Film[] tmp = new Film[length];
+        System.arraycopy(films, 0, tmp, 0, films.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = film;
+        films = tmp;
+    }
+
+    public Film[] showAll() {
+
+        int resultLength = films.length;
+        if (resultLength > limitForShow) {
+            resultLength = limitForShow;
+        } else {
+            resultLength = films.length;
+        }
+
+        Film[] result = new Film[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
+
+    public Film[] showAll(int limitForShow) {
+
+        int resultLength = films.length;
+        if (resultLength > limitForShow) {
+            resultLength = limitForShow;
+        } else {
+            resultLength = films.length;
+        }
+
+        Film[] result = new Film[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
+
+    public void removeById(int id) {
+        int length = films.length - 1;
+        Film[] tmp = new Film[length];
+        int index = 0;
+        for (Film film : films) {
+            if (film.getId() != id) {
+                tmp[index] = film;
+                index++;
+            }
+        }
+        films = tmp;
+    }
+}
---
+++ src/test/java/ru/netology/manager/FilmManagerTest.java
@@ -0,0 +1,106 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Film;
+import ru.netology.repository.FilmRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FilmManagerTest {
+
+    @Test
+    public void shouldShowAllFullFilms() {
+        Film first = new Film(1, "eger", "action");
+        Film second = new Film(2, "verr", "drama");
+        Film third = new Film(3, "dfer", "comedy");
+        Film fourth = new Film(4, "eger", "action");
+        Film fifth = new Film(5, "verr", "drama");
+        Film sixth = new Film(6, "dfer", "comedy");
+        Film seventh = new Film(7, "eger", "action");
+        Film eight = new Film(8, "verr", "drama");
+        Film ninth = new Film(9, "dfer", "comedy");
+        Film tenth = new Film(10, "eger", "action");
+        Film eleventh = new Film(11, "verr", "drama");
+        Film twelfth = new Film(12, "dfer", "comedy");
+
+        FilmManager manager = new FilmManager();
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eight);
+        manager.save(ninth);
+        manager.save(tenth);
+        manager.save(eleventh);
+        manager.save(twelfth);
+
+        Film[] actual = manager.showAll();
+        Film[] expected = {twelfth,eleventh,tenth,ninth,eight,seventh,sixth,fifth,fourth,third};
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldShowAllFullFilmsWithLimit() {
+        Film first = new Film(1, "eger", "action");
+        Film second = new Film(2, "verr", "drama");
+        Film third = new Film(3, "dfer", "comedy");
+        Film fourth = new Film(4, "eger", "action");
+        Film fifth = new Film(5, "verr", "drama");
+        Film sixth = new Film(6, "dfer", "comedy");
+        Film seventh = new Film(7, "eger", "action");
+        Film eight = new Film(8, "verr", "drama");
+        Film ninth = new Film(9, "dfer", "comedy");
+        Film tenth = new Film(10, "eger", "action");
+        Film eleventh = new Film(11, "verr", "drama");
+        Film twelfth = new Film(12, "dfer", "comedy");
+
+        FilmManager manager = new FilmManager();
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eight);
+        manager.save(ninth);
+        manager.save(tenth);
+        manager.save(eleventh);
+        manager.save(twelfth);
+
+        Film[] actual = manager.showAll(5);
+        Film[] expected = {twelfth,eleventh,tenth,ninth,eight};
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldShowAllOneFilm() {
+        Film first = new Film(1, "eger", "action");
+
+        FilmManager manager = new FilmManager();
+        manager.save(first);
+
+        Film[] actual = manager.showAll();
+        Film[] expected = {first};
+        assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldShowAllNoFilm() {
+
+        FilmManager manager = new FilmManager();
+
+        Film[] actual = manager.showAll();
+        Film[] expected = {};
+        assertArrayEquals(expected,actual);
+
+    }
+
+}
